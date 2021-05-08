da7342f152741073bf0d32416eba89461242a082
Mathias <47667093+Mojitas@users.noreply.github.com>
2021-05-08 15:19:48 +0200

    reupload
    Signed-off-by: Mathias <47667093+Mojitas@users.noreply.github.com>

--- config/swedish_keys.h
+++ config/swedish_keys.h
@@ -1,24 +1,19 @@
 
-#include <dt-bindings/zmk/keys.h>
-
 //some swedish key codes
 
-
-
-
-#define SE_LT NON_US_BSLH   // <
-#define SE_GT LS(SE_LT)     // >
-#define SE_PIPE RA(SE_LT)   // |
-#define SE_AP LS(N6)        // &
-#define SE_LBKT RA(N8)      // [  
-#define SE_RBKT RA(N9)      // ] 
-#define SE_LBRC RA(N7)      // {
-#define SE_RBRC RA(N0)      // }
-#define SE_LPAR LS(N8)      // ( 
-#define SE_RPAR LS(N9)      // ) 
-#define SE_FLSH LS(N7)      // /
-#define SE_BLSH RA(MINUS)   // \ 
-#define SE_AT RA(N2)        // @
-#define SE_EURO RA(N5)      // €
-#define SE_DQT  LS(N2)      // "
-#define SE_QT   BSLH
+#define SE_LT   NON_US_BSLH     // <
+#define SE_GT   LS(SE_LT)       // >
+#define SE_PIPE RA(SE_LT)       // |
+#define SE_AP   LS(N6)          // &
+#define SE_LBKT RA(N8)          // [  
+#define SE_RBKT RA(N9)          // ] 
+#define SE_LBRC RA(N7)          // {
+#define SE_RBRC RA(N0)          // }
+#define SE_LPAR LS(N8)          // ( 
+#define SE_RPAR LS(N9)          // ) 
+#define SE_FLSH LS(N7)          // /
+#define SE_BLSH RA(MINUS)       // \ 
+#define SE_AT   RA(N2)          // @
+#define SE_EURO RA(N5)          // €
+#define SE_DQT  LS(N2)          // "
+#define SE_QT   BSLH            // '   
