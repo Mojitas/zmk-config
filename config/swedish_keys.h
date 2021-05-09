89076aadbdfb211828607c706d03fa337221f2db
Mathias <47667093+Mojitas@users.noreply.github.com>
2021-05-09 20:34:30 +0200

    changed to lt
    Signed-off-by: Mathias <47667093+Mojitas@users.noreply.github.com>

--- config/swedish_keys.h
+++ config/swedish_keys.h
@@ -24,12 +24,13 @@
 #define SE_EURO  RA(N5)          // €
 #define SE_DQT   LS(N2)          // "
 #define SE_SQT   BSLH            // '   
-#define SE_TILDE RA(RBRC)        // ~
+#define SE_TILDE RA(RBKT)            // ~
 #define SE_STAR  PIPE            // *
 #define SE_MU    RA(M)           // µ
 #define SE_ACNT  EQUAL           // ´
 #define SE_CARET RBRC            // ^
 #define SE_EQUAL LS(N0)          // =
+#define SE_AT    RA(N2)
 #define SE_AO LBKT               // Å
 #define SE_AE SQT                // Ä
 #define SE_OE SEMI               // Ö
